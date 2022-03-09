a1649f6ea21ea818f582a8f36c07c889b67a7cb5
Miguel Oña Guerrero <miguelog3295@outlook.com>
2022-03-09 21:43:04 +0100

    Make method compute

--- src/main/java/org/miguel/Fibonacci.java
+++ src/main/java/org/miguel/Fibonacci.java
@@ -3,11 +3,24 @@ package org.miguel;
 public class Fibonacci {
 
     public int compute(int n){
+        int result;
+
         if(n < 0){
             throw new RuntimeException("The number is negative: " + n);
+        } else if(n == 0){
+            result = 0;
         }else {
+            int f0 = 0;
+            int f1 = 1;
+            int aux;
 
+            for(int i = 0; i < n; i++){
+                aux = f1;
+                f1 = f0 + f1;
+                f0 = aux;
+            }
+            result = f0;
         }
-        return 0;
+        return result;
     }
 }

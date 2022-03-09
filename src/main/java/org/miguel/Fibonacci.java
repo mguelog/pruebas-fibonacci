9e39473f94bdd9b3d1d91ed6fc8748989c3c0fe3
Miguel Oña Guerrero <miguelog3295@outlook.com>
2022-03-09 20:06:05 +0100

    Create test 1

--- src/main/java/org/miguel/Fibonacci.java
+++ src/main/java/org/miguel/Fibonacci.java
@@ -1,4 +1,8 @@
 package org.miguel;
 
 public class Fibonacci {
+
+    public int fibonacci(int n){
+        return 0;
+    }
 }
--- src/test/java/org/miguel/FibonacciTest.java
+++ src/test/java/org/miguel/FibonacciTest.java
@@ -1,4 +1,17 @@
 package org.miguel;
 
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class FibonacciTest {
+
+    @Test
+    public void shouldFibonacciReturnZeroIfNumberIsZero(){
+        Fibonacci fibonacci = new Fibonacci();
+
+        int expectedValue = 0;
+        int obtainedValue = fibonacci.fibonacci(0);
+
+        assertEquals(expectedValue, obtainedValue);
+    }
 }
